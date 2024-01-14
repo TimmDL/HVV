5415bffa3696589b69261de047c7baf82c030283
TimmDL <44569885+TimmDL@users.noreply.github.com>
2024-01-14 19:51:05 +0100

    Update HVVPlot.py

--- Code/HVVPlot.py
+++ Code/HVVPlot.py
@@ -2,15 +2,22 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+# Set the page to wide mode
+st.set_page_config(layout="wide")
+
+# Add title and description
+st.title('**Your Dashboard Title**')
+st.write('Description of the dashboard. Explains what it does, how to use it, etc.')
+
 # Read the CSV file
-df = pd.read_csv('Data/sst_alldata.csv')
+df = pd.read_csv('/Data/sst_alldata.csv')
 df['Timestamp'] = pd.to_datetime(df['Timestamp'])
 recent_stations = df.loc[df.groupby('Automatennr')['Timestamp'].idxmax()]
 
 # Define color for stations based on the simplified machine state
 color_scale = {
     'broken': 'red',
-    'warning': 'magenta',  
+    'warning': '#FFA15A',
     'operational': 'blue'
 }
 
@@ -29,12 +36,10 @@ def aggregate_info(group):
         color = 'operational'
     return info, color, size
 
-
-
 # Group by 'HstName' and aggregate data
 grouped = recent_stations.groupby('HstName').apply(lambda x: pd.Series({
     'Machine_Info': aggregate_info(x)[0],
-    'Station_Color': aggregate_info(x)[1],
+    'Station_Status': aggregate_info(x)[1],
     'Size': aggregate_info(x)[2],
     'Latitude': x['Latitude'].iloc[0],
     'Longitude': x['Longitude'].iloc[0]
@@ -42,7 +47,6 @@ grouped = recent_stations.groupby('HstName').apply(lambda x: pd.Series({
 
 # Checkbox for showing all LMU states
 show_all_states = st.checkbox('Show all LMU states')
-
 if show_all_states:
     # Plotting all states
     fig_all_states = px.scatter_mapbox(recent_stations,
@@ -54,13 +58,14 @@ if show_all_states:
                                        size_max=30,
                                        zoom=12,
                                        mapbox_style="open-street-map")
-    st.plotly_chart(fig_all_states)
+    fig_all_states.update_layout(height=600)
+    st.plotly_chart(fig_all_states, use_container_width=True)
 
 else:
     # Existing logic for simplified view
     grouped = recent_stations.groupby('HstName').apply(lambda x: pd.Series({
         'Machine_Info': aggregate_info(x)[0],
-        'Station_Color': aggregate_info(x)[1],
+        'Station_Status': aggregate_info(x)[1],
         'Size': aggregate_info(x)[2],
         'Latitude': x['Latitude'].iloc[0],
         'Longitude': x['Longitude'].iloc[0]
@@ -69,10 +74,47 @@ else:
     fig = px.scatter_mapbox(grouped,
                             lat="Latitude",
                             lon="Longitude",
-                            color='Station_Color',
+                            color='Station_Status',
                             size='Size',
                             color_discrete_map=color_scale,
                             size_max=30,
                             zoom=12,
                             mapbox_style="open-street-map")
-    st.plotly_chart(fig)
+    fig.update_layout(height=600)
+    st.plotly_chart(fig, use_container_width=True)
+
+# Search bar and column toggle
+search_query = st.text_input("Search:", "").lower()
+
+# Specify desired column order
+desired_order = ['Timestamp', 'state', 'LmuId', 'HstName', 'Standplatz', 'Linie',
+                 'LmuState', 'AlarmState', 'CompType', 'CompNr', 'prevState', 
+                 'comment', 'Automatennr', 'EVU / Bereich', 'Status Gerät','Latitude', 'Longitude']
+
+# Column toggle
+available_columns = recent_stations.columns.tolist()
+selected_columns = st.multiselect("Select columns to display:", available_columns, default=desired_order)
+
+# Filter Table by State
+st.write("Filter Table by State:")
+show_out_of_order = st.checkbox('Show Out of Order', True)
+show_warning = st.checkbox('Show Warning', True)
+show_operational = st.checkbox('Show Operational', True)
+
+filtered_states = []
+if show_out_of_order:
+    filtered_states.append('OUT_OF_ORDER')
+if show_warning:
+    filtered_states.append('WARNING')
+if show_operational:
+    filtered_states.append('OPERATIONAL')
+
+filtered_df = recent_stations[recent_stations['state'].isin(filtered_states)]
+filtered_df = filtered_df[filtered_df.apply(lambda row: row.astype(str).str.lower().str.contains(search_query).any(), axis=1)]
+filtered_df = filtered_df[selected_columns]
+
+# Apply the selected order to the dataframe for display
+filtered_df = filtered_df[selected_columns]
+
+# Display the filtered dataframe
+st.dataframe(filtered_df)
